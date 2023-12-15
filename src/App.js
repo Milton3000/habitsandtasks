a290c7911106afba3f2f43ab5aecb04a940dabc1
mazmann <sadi.lundberg@hotmail.com>
2023-12-15 20:46:38 +0100

    Basic habit functions in place

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Home from "./components/Home/Home";
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
+import Habits from "./components/Habits/Habits"
 
 function App() {
   return (
     <>
-      <Home />
-      {/* <Tasks /> */}
-      <NewTask />
+      <Habits />
     </>
   );
 }
--- src/components/Habits/Habits.jsx
+++ src/components/Habits/Habits.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Habits = () => {
-  return (
-    <div>Habits</div>
-  )
-}
+  let [habits, setHabits] = useState([
+    { habit: "Baers", streak: 2, priority: "low" },
+    { habit: "Jaern", streak: 2, priority: "high" },
+    { habit: "Baers", streak: 3, priority: "high" },
+  ]);
 
-export default Habits
+  let [priorityFilter, setPriorityFilter] = useState("all");
+  let [sortOrder, setSortOrder] = useState("asc");
+
+  let addHabit = () => {
+    let newHabit = { habit: "New Habit", streak: 0, priority: "medium" };
+    setHabits([...habits, newHabit]);
+  };
+
+  let increaseStreak = (index) => {
+    let updatedHabits = [...habits];
+    updatedHabits[index].streak += 1;
+    setHabits(updatedHabits);
+  };
+
+  let decreaseStreak = (index) => {
+    let updatedHabits = [...habits];
+    if (updatedHabits[index].streak > 0) {
+      updatedHabits[index].streak -= 1;
+    }
+    setHabits(updatedHabits);
+  };
+
+  let resetStreak = (index) => {
+    let updatedHabits = [...habits];
+    updatedHabits[index].streak = 0;
+    setHabits(updatedHabits);
+  };
+
+  let togglePriority = (index) => {
+    let updatedHabits = [...habits];
+    updatedHabits[index].priority = updatedHabits[index].priority === 'high' ? 'low' : 'high';
+    setHabits(updatedHabits);
+  };
+
+  let handleFilterChange = (event) => {
+    setPriorityFilter(event.target.value);
+  };
+
+  let handleSortChange = (event) => {
+    setSortOrder(event.target.value);
+  };
+
+  let filterByPriority = (habit) => {
+    if (priorityFilter === "all") {
+      return true;
+    }
+    return habit.priority === priorityFilter;
+  };
+
+  let compareStreaks = (a, b) => {
+    if (sortOrder === "asc") {
+      return a.streak - b.streak;
+    } else {
+      return b.streak - a.streak;
+    }
+  };
+
+  let sortedHabits = habits.filter(filterByPriority).sort(compareStreaks);
+
+  return (
+    <>
+      <h1>Habits</h1>
+      <div>
+
+        <label htmlFor="priorityFilter">Filter by Priority:</label>
+        <select id="priorityFilter" value={priorityFilter} onChange={handleFilterChange}>
+          <option value="all">All</option>
+          <option value="high">High Priority</option>
+          <option value="low">Low Priority</option>
+        </select>
+
+        <label htmlFor="sortOrder">Sort by Streaks:</label>
+        <select id="sortOrder" value={sortOrder} onChange={handleSortChange}>
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+
+      </div>
+      <button onClick={addHabit}>Add New Habit</button>
+      {sortedHabits.map((habit, index) => (
+        <div key={index}>
+          <p>{habit.habit}</p>
+          <p>
+            Streak: {habit.streak}
+            <button onClick={() => increaseStreak(index)}>+</button>
+            <button onClick={() => decreaseStreak(index)}>-</button>
+            <button onClick={() => resetStreak(index)}>Reset</button>
+          </p>
+          <p>
+            Priority: {habit.priority}
+            <button onClick={() => togglePriority(index)}>Switch Priority</button>
+          </p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Habits;

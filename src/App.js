8e228c24f819b3f58ddf16907a6a477a0ecd43a2
dadar870218 <dadar870218@gmail.com>
2023-12-15 17:22:56 +0100

    import and export friends

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Home from "./components/Home/Home";
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
+import Friends from "./components/Friends/Friends";
 
 function App() {
   return (
     <>
-      <Home />
-      {/* <Tasks /> */}
-      <NewTask />
+      <Friends />
     </>
   );
 }

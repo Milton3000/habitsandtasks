192b15a7c67df54ffe5ad9f3e5a28a9a08fa2ba6
Milton3000 <kristofferssonmilton@gmail.com>
2023-12-15 15:33:39 +0100

    New Tasks, fetch, form, jsx, states almost done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import Home from './components/Home/Home';
+import React from "react";
+import Home from "./components/Home/Home";
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
   return (
-<>
-
-<Home />
-
-</>
+    <>
+      <Home />
+      {/* <Tasks /> */}
+      <NewTask />
+    </>
   );
 }
 

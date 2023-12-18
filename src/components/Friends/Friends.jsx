88ec0a68cd8eba3f70502da2bc3977e97a0dd9c2
dadar870218 <dadar870218@gmail.com>
2023-12-18 21:24:06 +0100

    Filter (gender, min age , max age) added

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Friends.css";
 
-const FriendDetails = ({ friend, showMoreInfo, showHideInfo }) => (
+const FriendInfo = ({ friend, showMoreInfo, showHideInfo }) => (
   <div className="friend" onClick={showHideInfo}>
     <img
       className="avatar"
@@ -32,6 +32,12 @@ const Friends = () => {
   const [friends, setFriends] = useState([]);
   const [selectedFriend, setSelectedFriend] = useState(null);
 
+  const [filters, setFilters] = useState({
+    gender: "",
+    minAge: "",
+    maxAge: "",
+  });
+
   const fetchRandomUser = async () => {
     const response = await fetch("https://randomuser.me/api");
     const data = await response.json();
@@ -47,16 +53,50 @@ const Friends = () => {
     setFriends([...friends, newFriend]);
   };
 
+  const callSetFilters = () => {
+    setFilters({
+      ...filters,
+      gender: document.querySelector("#gender").value,
+      minAge: document.querySelector("#minAge").value,
+      maxAge: document.querySelector("#maxAge").value,
+    });
+  };
+
   const showHideInfo = (index) =>
     setSelectedFriend(selectedFriend === index ? null : index);
+
+  const filteredFriends = friends.filter((friend) => {
+    const friendAge =
+      new Date().getFullYear() - new Date(friend.dob).getFullYear();
+    return (
+      (filters.gender === "" || friend.gender === filters.gender) &&
+      (filters.minAge === "" || friendAge >= parseInt(filters.minAge)) &&
+      (filters.maxAge === "" || friendAge <= parseInt(filters.maxAge))
+    );
+  });
 
   return (
     <div>
       <button onClick={fetchRandomUser}>Lägg till ny vän</button>
+
+      <div>
+        <label>kön:</label>
+        <select id="gender">
+          <option value="">Alla</option>
+          <option value="male">Man</option>
+          <option value="female">Kvinna</option>
+        </select>
+        <label>Min ålder:</label>
+        <input id="minAge" type="number" />
+        <label>Max ålder:</label>
+        <input id="maxAge" type="number" />
+        <button onClick={callSetFilters}>Filter</button>
+      </div>
+
       <ul>
-        {friends.map((friend, index) => (
+        {filteredFriends.map((friend, index) => (
           <li key={index}>
-            <FriendDetails
+            <FriendInfo
               friend={friend}
               showMoreInfo={selectedFriend === index}
               showHideInfo={() => showHideInfo(index)}
